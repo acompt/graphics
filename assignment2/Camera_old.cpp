e9d7e663c498ecf93e4d51e337793989324f19bf
adamlpiel <adamlpiel@gmail.com>
2015-03-16 16:59:15 -0700

    this

--- assignment2/Camera_old.cpp
+++ assignment2/Camera_old.cpp
@@ -44,26 +44,33 @@ void Camera::Orient(Point& eye, Vector& look, Vector& up) {
 Matrix Camera::GetProjectionMatrix() {
 
 	
-	double c = (-1) * (this -> nearPlane / this -> farPlane);
+	// Rotation matrix
+	Matrix R = Matrix (u[0], u[1], u[2], 0,
+				 v[0], v[1], v[2], 0,
+				 w[0], w[1], w[2], 0,
+				 0, 0, 0, 1);
 
-	// The unhinge matrix
-	M1 = Matrix (1, 0, 0, 0,
-				 0, 1, 0, 0,
-				 0, 0, (1 / (1 + c)), (-1) * (c / (1 + c)), // Changed this back
-				 0, 0, (-1), 0);
+	Point Pn = eyePoint + (nearPlane * lookVector);
+	// Translation matrix
+	Matrix T = Matrix (1, 0, 0, (-1) * Pn[0],
+				0, 1, 0, (-1) * Pn[1],
+				0, 0, 1, (-1) * Pn[2],
+				0, 0, 0, 1);
 
-	// Scale matrix
-	double m2e11, m2e22, m2e33;
-	m2e11 = 1 / (this->farPlane * tan(viewWidthAngle / 2));
-	m2e22 = 1 / (this->farPlane * tan(viewHeightAngle / 2));
-	m2e33 = 1 / farPlane;
-	M2 = Matrix (m2e11, 0, 0, 0,
-				  0, m2e22, 0, 0,
-				  0, 0, m2e33, 0,
-				  0, 0, 0, 1);
+	// Scaling matrix
+	int x = 2/screenWidth;
+	int y = 2/screenHeight;
+	Matrix S = Matrix (x, 0, 0, 0,
+				0, y, 0, 0,
+				0, 0, farPlane, 0,
+				0, 0, 0, 1);
+
+	
+	//pjM = S;
 
 
-	return M1 * M2;
+
+	return pjM;
 }
 
 
@@ -98,6 +105,25 @@ void Camera::SetScreenSize (int screenWidth, int screenHeight) {
 
 Matrix Camera::GetModelViewMatrix() {
 
+	double c = (-1) * (this -> nearPlane / this -> farPlane);
+
+	// The unhinge matrix
+	M1 = Matrix (1, 0, 0, 0,
+				 0, 1, 0, 0,
+				 0, 0, (1 / (1 + c)), (-1) * (c / (1 + c)), // Changed this back
+				 0, 0, (-1), 0);
+
+	// Scale matrix
+	double m2e11, m2e22, m2e33;
+	m2e11 = 1 / (this->farPlane * tan(viewWidthAngle / 2));
+	m2e22 = 1 / (this->farPlane * tan(viewHeightAngle / 2));
+	m2e33 = 1 / farPlane;
+	M2 = Matrix (m2e11, 0, 0, 0,
+				  0, m2e22, 0, 0,
+				  0, 0, m2e33, 0,
+				  0, 0, 0, 1);
+
+
 
 	//Rotation matrix
 	M3 = Matrix ( u[0], u[1], u[2], 0,
@@ -113,7 +139,7 @@ Matrix Camera::GetModelViewMatrix() {
 				0, 0, 0, 1);
 
 	// M1 and M3 do something! We're on the right track!
-	mvM = M3 * M4;
+	mvM = M1 * M2 * M3 * M4;
 	return mvM;
 
 }
@@ -126,28 +152,26 @@ Matrix Camera::RotateArbVec(double angle, Vector a) {
 	M2 = rotZ_mat((-1)*atan(a[1]/sqrt(a[0]*a[0] + a[2]*a[2])));
 	M3 = rotX_mat(angle);
 
-	M1i = inv_rotY_mat(atan(a[2] / a[0]));
-	M2i = inv_rotZ_mat((-1)*atan(a[1]/sqrt(a[0]*a[0] + a[2]*a[2])));
-
 	R = M1i * M2i * M3 * M2 * M1;
 
 	return R;
 }
 
 void Camera::RotateV(double angle) {
-	//return;
-	Matrix R = RotateArbVec(angle, v);
-	//lookVector = R * lookVector;
-	w = R * w;
-	v = R * v;
-	//Orient(eyePoint, lookVector, upVector);
 
+	return; // remove to try using below code
+
+	Matrix R = RotateArbVec(angle, v);
+	//w = R*w;
+	//u = R*u;
+
+	//pjM = R * pjM;
 
 }
 
 void Camera::RotateU(double angle) {
 
-	//return; // remove to try using below code
+	return; // remove to try using below code
 
 	Matrix R = RotateArbVec(angle, u);
 	v = R*v;
@@ -157,11 +181,11 @@ void Camera::RotateU(double angle) {
 
 void Camera::RotateW(double angle) {
 
-	//return; // remove to try using below code
+	return; // remove to try using below code
 
 	Matrix R = RotateArbVec(angle, w);
-	u = R * u;
-	v = R * v;
+	u = R*u;
+	v = R*v;
 
 }
 
